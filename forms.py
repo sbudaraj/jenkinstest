5c819293f70340655516b7b28f940448f0346db7
Siva <sivakumar.budaraju@gmail.com>
2018-08-02 14:59:33 -0700

    "Test Add"

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from flask_table import Table, Col	
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+
+class ItemForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    quantity = StringField('quantity', validators=[DataRequired()])
+    description = StringField('description', validators=[DataRequired()])
